3933ceb7c0db1ba0ee3a480b571ca9dcdd1a6169
JeffPerk <jeff.perk87@gmail.com>
2016-07-20 10:49:37 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>To Do List</title>
+    <link rel="stylesheet" href="css/style.css" charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+
+
+
+  </head>
+  <body>
+    <div class="main-container">
+      <div class="form-container">
+        <div class="title-box">
+          <h1>To Do List</h1>
+        </div>
+        <div class="form-section">
+          <form class="form" action="index.php" method="post">
+            <input id="text-field" type="text" name="todo" placeholder="What to do...">
+            <input id="submit-button" type="submit" name="submit" value="Submit">
+          </form>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
